ab0f7947cb6f214f00b4f67a8b449688beb4c7b0
Ahmet Çandıroğlu <ahmetcandiroglu@gmail.com>
2018-12-12 20:34:31 +0300

    Introduce view-utilities

--- app/tab-manager.js
+++ app/tab-manager.js
@@ -1,5 +1,5 @@
 import properties from "../assets/generated/properties";
-
+import {initFilterTabView, renderObjectProps} from "./view-utilities";
 
 export default class TabManager {
 
@@ -18,38 +18,7 @@ export default class TabManager {
         const nodeProps = this.nodeProperties;
         const edgeProps = this.edgeProperties;
 
-        let filterRow = $('<div class="row"></div>');
-        let nodeCol = $('<div class="col-6" id="filter-col-node"><p class="text-center">Nodes</p></div>');
-        let nodeAttr = $('<div><hr></div>');
-        let edgeCol = $('<div class="col-6" id="filter-col-edge"><p class="text-center">Edges</p></div>');
-        let edgeAttr = $('<div><hr></div>');
-
-        // Generate filter elements for node properties
-        for(const prop in nodeProps){
-            let filterBtn = $('<input type="button" class="btn btn-info filter-btn">');
-            filterBtn.prop({
-                id: prop + 'Check',
-                value: prop
-            });
-
-            nodeCol.append(filterBtn);
-        }
-
-        // Generate filter elements for edge properties
-        for(const prop in edgeProps){
-            let filterBtn = $('<input type="button" class="btn btn-info filter-btn">');
-            filterBtn.prop({
-                id: prop + 'Check',
-                value: prop
-            });
-
-            edgeCol.append(filterBtn);
-        }
-
-        nodeCol.append(nodeAttr);
-        edgeCol.append(edgeAttr);
-        filterRow.append(nodeCol, edgeCol);
-        $('#filter').append(filterRow);
+        initFilterTabView(nodeProps, edgeProps);
     }
 
     bindFilterByClassFunctionality(cb){
@@ -61,9 +30,7 @@ export default class TabManager {
 
         let props = getCommonObjectProps(event.target);
 
-        let htmlToRender = renderCommonObjectProps(props);
-
-        $('#object').html(htmlToRender);
+        renderObjectProps(props);
     }
 
     hideObjectProps(event){}
@@ -111,36 +78,6 @@ function getCommonObjectProps(eleList){
     });
 
     return props;
-}
-
-function renderCommonObjectProps(props){
-    let classNames = "";
-    if(props.classNames && props.classNames.length > 0)
-        classNames = props.classNames.join(' & ');
-
-    let propList = '<ul class="list-group list-group-flush">' +
-        '<li class="list-group-item text-center"><b>' + classNames + '</b></li>';
-
-    for(const key in props){
-        if(key === 'classNames')
-            continue;
-
-        // Replace - and _ with space
-        let renderedKey = key.replace(/[_\-]/g, " ");
-        let renderedValue = props[key];
-
-        if(renderedValue instanceof Date){
-            const options = { weekday: undefined, year: 'numeric', month: 'long', day: 'numeric' };
-            renderedValue = renderedValue.toLocaleDateString('en-EN', options);
-        }
-
-        propList += '<li class="list-group-item"><div class="row">' +
-            '<div class="col-4 offset-2">' + renderedKey + '</div>' +
-            '<div class="col-6">' + renderedValue + '</div></div></li>';
-    }
-    propList += '</ul><br>';
-
-    return propList;
 }
 
 // Handle Tab functionality
