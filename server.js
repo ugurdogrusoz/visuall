be524198a045f763b483757b63918d938bea0e38
canbax <yusufsaidcanbaz@gmail.com>
2019-07-25 13:38:38 +0300

    change port number for convenience

--- server.js
+++ server.js
@@ -8,12 +8,11 @@ const appPath = '/dist/ng-visuall';
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + appPath));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname + appPath + '/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + appPath + '/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 4200;
 app.listen(port);
 console.log('server listening port: ', port);
