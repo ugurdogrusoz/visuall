b5de08263103b4d5c098ad6a67056ee416c76f73
canbax <yusufsaidcanbaz@gmail.com>
2019-11-04 12:20:29 +0300

    docker script added for the record

---
+++ src/assets/bash_script/docker_scripts.sh
@@ -0,0 +1,3 @@
+# docker generated neo4j databases will restart themselves after reboot
+sudo docker run --name hydra --restart unless-stopped -p7474:7474 -p7687:7687 -d -v /home/newt-beta-server/neo4j_databases/hydra/data:/data -v /home/newt-beta-server/neo4j_databases/hydra/logs:/logs -v /home/newt-beta-server/neo4j_databases/hydra/conf:/conf  --env NEO4J_AUTH=neo4j/123 neo4j
+sudo docker run --name visuall --restart unless-stopped -p3001:7474 -p3002:7473 -p3003:7687 -d -v /home/newt-beta-server/neo4j_databases/visuall/data:/data -v /home/newt-beta-server/neo4j_databases/visuall/logs:/logs -v /home/newt-beta-server/neo4j_databases/visuall/conf:/conf --env NEO4J_AUTH=none neo4j
