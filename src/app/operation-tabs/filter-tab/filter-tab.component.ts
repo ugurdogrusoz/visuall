cf1fdba3529afc3a1aaf7563c10d14275fa598e5
canbax <yusufsaidcanbaz@gmail.com>
2019-12-16 09:14:03 +0300

    #149 fix to filter heterogenous data

--- src/app/operation-tabs/filter-tab/filter-tab.component.ts
+++ src/app/operation-tabs/filter-tab/filter-tab.component.ts
@@ -228,13 +228,36 @@ export class FilterTabComponent implements OnInit {
     if (!data.data[0][1]) {
       return;
     }
-    this.tableInput.columns = Object.keys(data.data[0][1]);
-    this.tableInput.isNodeData = !this.filteringRule.isEdge;
-    for (let i = 0; i < data.data.length; i++) {
-      let d: iTableData[] = [{ val: data.data[i][0], type: TableDataType.number }];
 
+    this.tableInput.isNodeData = !this.filteringRule.isEdge;
+    let keySet = new Set<string>();
+
+    for (let i = 0; i < data.data.length; i++) {
       for (let [k, v] of Object.entries(data.data[i][1])) {
-        d.push(this.rawData2TableData(k, v));
+        keySet.add(k);
+      }
+    }
+
+    this.tableInput.columns = [...keySet];
+
+    for (let i = 0; i < data.data.length; i++) {
+      // first column is ID
+      let d: iTableData[] = [{ val: data.data[i][0], type: TableDataType.string }];
+      // j is column index
+      let j = 0;
+      for (let [k, v] of Object.entries(data.data[i][1])) {
+        let idx = this.tableInput.columns.indexOf(k);
+        if (idx > -1) {
+          d[idx + 1] = this.rawData2TableData(k, v);
+        } else {
+          d[j] = { val: '', type: TableDataType.string };
+        }
+        j++;
+      }
+      for (j = 0; j < d.length; j++) {
+        if (!d[j]) {
+          d[j] = { val: '', type: TableDataType.string };
+        }
       }
       this.tableInput.results.push(d)
     }
