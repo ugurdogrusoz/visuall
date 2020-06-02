6cb73d8b14d7e0fd3bb3bbe6a25bd4e9eb70eca1
canbax <yusufsaidcanbaz@gmail.com>
2020-06-02 09:02:37 +0300

    #172 more stable cise layout

--- src/app/cytoscape.service.ts
+++ src/app/cytoscape.service.ts
@@ -53,74 +53,7 @@ export class CytoscapeService {
     // use cise layout algorithm
     cytoscape.use(cise);
 
-    this._g.layout = {
-      name: 'fcose',
-      // 'draft', 'default' or 'proof' 
-      // - 'draft' only applies spectral layout 
-      // - 'default' improves the quality with incremental layout (fast cooling rate)
-      // - 'proof' improves the quality with incremental layout (slow cooling rate) 
-      quality: 'default',
-      // use random node positions at beginning of layout
-      // if this is set to false, then quality option must be 'proof'
-      randomize: false,
-      // whether or not to animate the layout
-      animate: true,
-      // duration of animation in ms, if enabled
-      animationDuration: 1000,
-      // easing of animation, if enabled
-      animationEasing: undefined,
-      // fit the viewport to the repositioned nodes
-      fit: true,
-      // padding around layout
-      padding: 10,
-      // whether to include labels in node dimensions. Valid in 'proof' quality
-      nodeDimensionsIncludeLabels: false,
-
-      /* spectral layout options */
-
-      // false for random, true for greedy sampling
-      samplingType: true,
-      // sample size to construct distance matrix
-      sampleSize: 25,
-      // separation amount between nodes
-      nodeSeparation: 75,
-      // power iteration tolerance
-      piTol: 0.0000001,
-
-      /* incremental layout options */
-
-      // Node repulsion (non overlapping) multiplier
-      nodeRepulsion: 4500,
-      // Ideal edge (non nested) length
-      idealEdgeLength: 50,
-      // Divisor to compute edge forces
-      edgeElasticity: 0.45,
-      // Nesting factor (multiplier) to compute ideal edge length for nested edges
-      nestingFactor: 0.1,
-      // Gravity force (constant)
-      gravity: 0.25,
-      // Maximum number of iterations to perform
-      numIter: 2500,
-      // For enabling tiling
-      tile: false,
-      // Represents the amount of the vertical space to put between the zero degree members during the tiling operation(can also be a function)
-      tilingPaddingVertical: 10,
-      // Represents the amount of the horizontal space to put between the zero degree members during the tiling operation(can also be a function)
-      tilingPaddingHorizontal: 10,
-      // Gravity range (constant) for compounds
-      gravityRangeCompound: 1.5,
-      // Gravity force (constant) for compounds
-      gravityCompound: 1.0,
-      // Gravity range (constant)
-      gravityRange: 3.8,
-      // Initial cooling factor for incremental layout  
-      initialEnergyOnIncremental: 0.3,
-
-      /* layout event callbacks */
-      ready: () => { }, // on layoutready
-      stop: () => { } // on layoutstop
-    };
-
+    this._g.layout = this._g.getFcoseOptions();
     this._g.cy = cytoscape({
       container: containerElem,
       style: stylesheet,
@@ -878,7 +811,6 @@ export class CytoscapeService {
         clusters[i].move({ parent: parentNode.data.id });
       }
     } else {
-      this._g.layout.name = 'cise';
       let arr = [];
       for (let i = 0; i < clusters.length; i++) {
         let a = [];
@@ -888,6 +820,7 @@ export class CytoscapeService {
         arr.push(a);
       }
       this._g.layout.clusters = arr;
+      this._g.layout = this._g.getCiseOptions(arr);
     }
   }
 
@@ -908,8 +841,15 @@ export class CytoscapeService {
         this._g.cy.$('#' + n).move({ parent: 'c' + clustering[n] });
       }
     } else {
-      this._g.layout.name = 'cise';
-      this._g.layout.clusters = (node) => { return clustering[node.id()]; };
+      // let arr = [];
+      // for (let i in clusters) {
+      //   arr.push([]);
+      // }
+      // for (let i in clustering) {
+      //   arr[clustering[i]].push(i);
+      // }
+      // this._g.layout = this._g.getCiseOptions(arr);
+      this._g.layout = this._g.getCiseOptions((node) => { return clustering[node.id()]; });
     }
   }
 
--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -89,9 +89,7 @@ export class GlobalVariableService {
       this.statusMsg.next('Performing layout...');
     }
     this.setLoadingStatus(true);
-    if (this.userPrefs.groupingOption.getValue() != GroupingOptionTypes.clusterId) {
-      this.layout.name = 'fcose';
-    }
+
     elems4layout.layout(this.layout).run();
     this.statusMsg.next('Rendering graph...');
   }
@@ -100,6 +98,9 @@ export class GlobalVariableService {
     if (!this.userPrefs.isAutoIncrementalLayoutOnChange.getValue() && !isRandomize && !isDirectCommand) {
       this.cy.fit();
       return;
+    }
+    if (this.userPrefs.groupingOption.getValue() != GroupingOptionTypes.clusterId) {
+      this.layout = this.getFcoseOptions();
     }
     this.layout.animationDuration = animationDuration;
     this.switchLayoutRandomization(isRandomize);
@@ -205,4 +206,150 @@ export class GlobalVariableService {
       this.setLoadingStatus(false);
     });
   }
+
+  getFcoseOptions() {
+    return {
+      name: 'fcose',
+      // 'draft', 'default' or 'proof' 
+      // - 'draft' only applies spectral layout 
+      // - 'default' improves the quality with incremental layout (fast cooling rate)
+      // - 'proof' improves the quality with incremental layout (slow cooling rate) 
+      quality: 'default',
+      // use random node positions at beginning of layout
+      // if this is set to false, then quality option must be 'proof'
+      randomize: false,
+      // whether or not to animate the layout
+      animate: true,
+      // duration of animation in ms, if enabled
+      animationDuration: 1000,
+      // easing of animation, if enabled
+      animationEasing: undefined,
+      // fit the viewport to the repositioned nodes
+      fit: true,
+      // padding around layout
+      padding: 10,
+      // whether to include labels in node dimensions. Valid in 'proof' quality
+      nodeDimensionsIncludeLabels: false,
+
+      /* spectral layout options */
+
+      // false for random, true for greedy sampling
+      samplingType: true,
+      // sample size to construct distance matrix
+      sampleSize: 25,
+      // separation amount between nodes
+      nodeSeparation: 75,
+      // power iteration tolerance
+      piTol: 0.0000001,
+
+      /* incremental layout options */
+
+      // Node repulsion (non overlapping) multiplier
+      nodeRepulsion: 4500,
+      // Ideal edge (non nested) length
+      idealEdgeLength: 50,
+      // Divisor to compute edge forces
+      edgeElasticity: 0.45,
+      // Nesting factor (multiplier) to compute ideal edge length for nested edges
+      nestingFactor: 0.1,
+      // Gravity force (constant)
+      gravity: 0.25,
+      // Maximum number of iterations to perform
+      numIter: 2500,
+      // For enabling tiling
+      tile: false,
+      // Represents the amount of the vertical space to put between the zero degree members during the tiling operation(can also be a function)
+      tilingPaddingVertical: 10,
+      // Represents the amount of the horizontal space to put between the zero degree members during the tiling operation(can also be a function)
+      tilingPaddingHorizontal: 10,
+      // Gravity range (constant) for compounds
+      gravityRangeCompound: 1.5,
+      // Gravity force (constant) for compounds
+      gravityCompound: 1.0,
+      // Gravity range (constant)
+      gravityRange: 3.8,
+      // Initial cooling factor for incremental layout  
+      initialEnergyOnIncremental: 0.3,
+
+      /* layout event callbacks */
+      ready: () => { }, // on layoutready
+      stop: () => { } // on layoutstop
+    };
+  }
+
+  getCiseOptions(clusterer) {
+    return {
+      // -------- Mandatory parameters --------
+      name: 'cise',
+
+      // ClusterInfo can be a 2D array contaning node id's or a function that returns cluster ids. 
+      // For the 2D array option, the index of the array indicates the cluster ID for all elements in 
+      // the collection at that index. Unclustered nodes must NOT be present in this array of clusters.
+      // 
+      // For the function, it would be given a Cytoscape node and it is expected to return a cluster id  
+      // corresponding to that node. Returning negative numbers, null or undefined is fine for unclustered
+      // nodes.  
+      // e.g
+      // Array:                                     OR          function(node){
+      //  [ ['n1','n2','n3'],                                       ...
+      //    ['n5','n6']                                         }
+      //    ['n7', 'n8', 'n9', 'n10'] ]                         
+      clusters: clusterer,
+
+      // -------- Optional parameters --------
+      // Whether to animate the layout
+      // - true : Animate while the layout is running
+      // - false : Just show the end result
+      // - 'end' : Animate directly to the end result
+      animate: 'end',
+
+      // number of ticks per frame; higher is faster but more jerky
+      refresh: 10,
+
+      // Animation duration used for animate:'end'
+      animationDuration: undefined,
+
+      // Easing for animate:'end'
+      animationEasing: undefined,
+
+      // Whether to fit the viewport to the repositioned graph
+      // true : Fits at end of layout for animate:false or animate:'end'
+      fit: true,
+
+      // Padding in rendered co-ordinates around the layout
+      padding: 30,
+
+      // separation amount between nodes in a cluster
+      // note: increasing this amount will also increase the simulation time 
+      nodeSeparation: 12.5,
+
+      // Inter-cluster edge length factor 
+      // (2.0 means inter-cluster edges should be twice as long as intra-cluster edges)
+      idealInterClusterEdgeLengthCoefficient: 1.4,
+
+      // Whether to pull on-circle nodes inside of the circle
+      allowNodesInsideCircle: false,
+
+      // Max percentage of the nodes in a circle that can move inside the circle
+      maxRatioOfNodesInsideCircle: 0.1,
+
+      // - Lower values give looser springs
+      // - Higher values give tighter springs
+      springCoeff: 0.45,
+
+      // Node repulsion (non overlapping) multiplier
+      nodeRepulsion: 4500,
+
+      // Gravity force (constant)
+      gravity: 0.25,
+
+      // Gravity range (constant)
+      gravityRange: 3.8,
+
+      // Layout event callbacks; equivalent to `layout.one('layoutready', callback)` for example
+      ready: function () { }, // on layoutready
+      stop: function () { }, // on layoutstop
+    };
+  }
+
 }
