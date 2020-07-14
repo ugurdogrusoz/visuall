8836aab79ea269156fa660a6df7ab7ac3fef9c2c
canbax <yusufsaidcanbaz@gmail.com>
2020-07-14 12:31:25 +0300

    #240 put section in UI

--- src/app/operation-tabs/query-tab/query-tab.component.ts
+++ src/app/operation-tabs/query-tab/query-tab.component.ts
@@ -9,17 +9,27 @@ export class QueryTabComponent implements OnInit {
   queryTypes: string[];
   selectedQuery: string;
   selectedIdx: number;
+  advancedQueries: string[];
+  selectedAdvancedQuery: string;
+  selectedIdx2: number;
 
   constructor() {
     this.queryTypes = ['Get actors by title counts', 'Get titles by genre'];
+    this.advancedQueries = ['Get graph of interest', 'Get common targets/regulators'];
     this.selectedIdx = -1;
+    this.selectedIdx2 = -1;
   }
 
   ngOnInit() {
     this.selectedQuery = '';
+    this.selectedAdvancedQuery = '';
   }
 
   changeQuery(event) {
     this.selectedIdx = this.queryTypes.findIndex(x => x == event.target.value);
+  }
+
+  changeAdvancedQuery(event) {
+    this.selectedIdx2 = this.advancedQueries.findIndex(x => x == event.target.value);
   }
 }
