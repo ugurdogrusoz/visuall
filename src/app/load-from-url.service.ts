8e4e4bb3479772efe0f95b6e08233f3269e798ac
H. Eren Calik <eren.calik@gmail.com>
2020-08-04 12:38:37 +0300

    Tab indentation resolved

--- src/app/load-from-url.service.ts
+++ src/app/load-from-url.service.ts
@@ -4,36 +4,35 @@ import { CytoscapeService } from './cytoscape.service';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 
 export class URLLoadService {
 
-    url: string;
-    data: any;
-    fileRead : boolean = false;
+  url: string;
+  data: any;
+  fileRead: boolean = false;
 
-    constructor(private route: ActivatedRoute, private http:HttpClient,
-      private _cyService: CytoscapeService){}
+  constructor(private route: ActivatedRoute, private http: HttpClient,
+    private _cyService: CytoscapeService) { }
 
-    init() {
-        this.route.queryParams.subscribe(async params => {
-            //if there is a URL query, getData() is invoked, otherwise nothing
-            this.url = await params['URL'];
-            this.getData();
-        });
-    }
-
-    getData() {
-
-      this.http.get<any>(this.url, { responseType: 'blob' as 'json' }).subscribe(async data => {
-          this.data = new File([await data], 'Graph', { type: 'txt', lastModified:Date.now()});
-          //data is taken as json and given to cytoscape's loadFile method as it is in navbar load
-          this._cyService.loadFile(this.data);
-      },
-      (err: HttpErrorResponse) => {
-          console.log (err.message);
-      }
-      );
+  init() {
+    this.route.queryParams.subscribe(async params => {
+      //if there is a URL query, getData() is invoked, otherwise nothing
+      this.url = await params['URL'];
+      this.getData();
+    });
   }
 
+  getData() {
+
+    this.http.get<any>(this.url, { responseType: 'blob' as 'json' }).subscribe(async data => {
+      this.data = new File([await data], 'Graph', { type: 'txt', lastModified: Date.now() });
+      //data is taken as json and given to cytoscape's loadFile method as it is in navbar load
+      this._cyService.loadFile(this.data);
+    },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
+  }
 }
