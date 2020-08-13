ca942fbff4d670c92b2e3e873810e1c45dbf51f2
msalihaltun <muhammedsalihaltun@gmail.com>
2020-08-13 10:19:31 +0300

    small mistake

--- src/app/cytoscape/cytoscape.component.ts
+++ src/app/cytoscape/cytoscape.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener} from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CytoscapeService } from '../cytoscape.service';
 import { TimebarService } from '../timebar.service';
 import { ContextMenuService } from '../context-menu/context-menu.service';
