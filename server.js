b056c6113e5040b44e08fbca722d9d6692669fc6
H. Eren Calik <eren.calik@gmail.com>
2020-08-17 12:35:29 +0300

    Loading from external source updated
    Because of the cors problem, the data request and recieve-send part is transferred to server-side, backend.

--- server.js
+++ server.js
@@ -1,18 +1,47 @@
 //Install express server
 const express = require('express');
+const http = require('http');
+const url = require('url');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text({ type: 'text/*' }))
+
+
 const appPath = '/dist/ng-visuall';
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + appPath));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + appPath + '/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
 const port = process.env.PORT || 4200;
 app.listen(port);
+
+app.get('/urlquery/*', function (req, res) {
+	var reqURL = req.url.substr(10);
+	var data = [];
+	var request = http.request(reqURL, function (response, body) {
+		response.on('data', function (chunk) {
+			 data.push(chunk);
+		});
+		response.on('end', function () {
+		var result = JSON.parse(data.join(''))
+		res.send(result);
+
+		});
+	});
+	request.on('error', function (e) {
+		console.log(e.message);
+	});
+	request.end();
+});
+
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname + appPath + '/index.html'));
+});
+
+
+// Start the app by listening on the default Heroku port
+
 console.log('server listening port: ', port);
