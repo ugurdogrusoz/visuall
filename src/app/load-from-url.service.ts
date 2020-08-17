d14b8cb6497f26d1e8767133b773384d77d5dd60
H. Eren Calik <eren.calik@gmail.com>
2020-08-17 12:00:43 +0300

    Loading from external source updated
    Because of the cors problem, the data request and recieve-send part is transferred to server-side, backend.

--- src/app/load-from-url.service.ts
+++ src/app/load-from-url.service.ts
@@ -16,6 +16,7 @@ export class URLLoadService {
     private _cyService: CytoscapeService) { }
 
   init() {
+
     this.route.queryParams.subscribe(async params => {
       //if there is a URL query, getData() is invoked, otherwise nothing
       this.url = await params['URL'];
@@ -24,15 +25,26 @@ export class URLLoadService {
   }
 
   getData() {
+    if (typeof this.url !== 'undefined') {
+      //if url is not null, get the data from the given route else wait for 200 ms and try again
+      this.http.get<any>(('urlquery/' + this.url), {
+        headers: { 'Content-Type': 'application/json' }
+        , observe: "body", responseType: "text" as "json"
+      }).subscribe(async data => {
 
-    this.http.get<any>(this.url, { responseType: 'blob' as 'json' }).subscribe(async data => {
-      this.data = new File([await data], 'Graph', { type: 'txt', lastModified: Date.now() });
-      //data is taken as json and given to cytoscape's loadFile method as it is in navbar load
-      this._cyService.loadFile(this.data);
-    },
-      (err: HttpErrorResponse) => {
-        console.log(err.message);
-      }
-    );
+        this.data = new File([await data], 'Graph', { type: 'text', lastModified: Date.now() });
+
+        //data is taken as json and given to cytoscape's loadFile method as it is in navbar load
+        this._cyService.loadFile(this.data);
+      },
+        (err: HttpErrorResponse) => {
+          console.log(err.message);
+        }
+      );
+    }
+    else {
+      setTimeout(this.getData, 200);
+    }
   }
+
 }
