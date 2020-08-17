50e6f45185eee383b08ae5617dbb04ff1bdb861f
H. Eren Calik <eren.calik@gmail.com>
2020-08-17 12:43:05 +0300

    Formatting Update

--- src/app/load-from-url.service.ts
+++ src/app/load-from-url.service.ts
@@ -4,45 +4,47 @@ import { CytoscapeService } from './cytoscape.service';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 
 export class URLLoadService {
 
-    url: string ;
-    data: any;
-    fileRead : boolean = false;
+  url: string;
+  data: any;
+  fileRead: boolean = false;
 
-    constructor(private route: ActivatedRoute, private http:HttpClient,
-      private _cyService: CytoscapeService){}
+  constructor(private route: ActivatedRoute, private http: HttpClient,
+    private _cyService: CytoscapeService) { }
 
-    init() {
+  init() {
 
-        this.route.queryParams.subscribe(async params => {
-            //if there is a URL query, getData() is invoked, otherwise nothing
-            this.url = await params['URL'];
-            this.getData();
-        });
+    this.route.queryParams.subscribe(async params => {
+      //if there is a URL query, getData() is invoked, otherwise nothing
+      this.url = await params['URL'];
+      this.getData();
+    });
+  }
+
+  getData() {
+    if (typeof this.url !== 'undefined') {
+      //if url is not null, get the data from the given route else wait for 200 ms and try again
+      this.http.get<any>(('urlquery/' + this.url), {
+        headers: { 'Content-Type': 'application/json' }
+        , observe: "body", responseType: "text" as "json"
+      }).subscribe(async data => {
+
+        this.data = new File([await data], 'Graph', { type: 'text', lastModified: Date.now() });
+
+        //data is taken as json and given to cytoscape's loadFile method as it is in navbar load
+        this._cyService.loadFile(this.data);
+      },
+        (err: HttpErrorResponse) => {
+          console.log(err.message);
+        }
+      );
     }
-
-    getData() {
-        if(typeof this.url !== 'undefined'){
-                    //if url is not null, get the data from the given route else wait for 200 ms and try again
-          this.http.get<any>(('urlquery/'+this.url),{headers: {'Content-Type': 'application/json'}
-            , observe: "body", responseType: "text" as "json"}).subscribe(async data => {
-
-            this.data = new File([await data], 'Graph', { type: 'text', lastModified:Date.now()});
-
-              //data is taken as json and given to cytoscape's loadFile method as it is in navbar load
-            this._cyService.loadFile(this.data);
-          },
-          (err: HttpErrorResponse) => {
-              console.log (err.message);
-          }
-          );
-      }
-      else{
-        setTimeout(this.getData, 200);
-      }
+    else {
+      setTimeout(this.getData, 200);
     }
+  }
 
 }
