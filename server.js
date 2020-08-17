4a4241c5f9d87209ad54b45a4d70100e1ae65d5d
H. Eren Calik <eren.calik@gmail.com>
2020-08-17 12:03:34 +0300

    Revert "Loading from external source updated"
    This reverts commit d14b8cb6497f26d1e8767133b773384d77d5dd60.

--- server.js
+++ server.js
@@ -1,48 +1,18 @@
-//Install necessary tools
+//Install express server
 const express = require('express');
-const http = require('http');
-const url = require('url');
-const bodyParser = require('body-parser');
 const path = require('path');
 
-//use express server as app
 const app = express();
 
-//use parsers for data get request
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text({ type: 'text/*' }))
-
 const appPath = '/dist/ng-visuall';
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + appPath));
 
-// Start the app by listening on the default Heroku port
-const port = process.env.PORT || 4200;
-app.listen(port);
-
-//get the requested data if there is a get request from frontend and respond with the data in url
-app.get('/urlquery/*', function (req, res) {
-  var reqURL = req.url.substr(10);
-  var data = [];
-  var request = http.request(reqURL, function (response, body) {
-    response.on('data', function (chunk) {
-      data.push(chunk);
-    });
-    response.on('end', function () {
-      var result = JSON.parse(data.join(''))
-      res.send(result);
-    });
-  });
-  request.on('error', function (e) {
-    console.log(e.message);
-  });
-  request.end();
-});
-
-//send user to index from any path except /urlquery
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + appPath + '/index.html'));
 });
 
-//log that server has started listening
+// Start the app by listening on the default Heroku port
+const port = process.env.PORT || 4200;
+app.listen(port);
 console.log('server listening port: ', port);
--- src/app/load-from-url.service.ts
+++ src/app/load-from-url.service.ts
@@ -16,7 +16,6 @@ export class URLLoadService {
     private _cyService: CytoscapeService) { }
 
   init() {
-
     this.route.queryParams.subscribe(async params => {
       //if there is a URL query, getData() is invoked, otherwise nothing
       this.url = await params['URL'];
@@ -25,26 +24,15 @@ export class URLLoadService {
   }
 
   getData() {
-    if (typeof this.url !== 'undefined') {
-      //if url is not null, get the data from the given route else wait for 200 ms and try again
-      this.http.get<any>(('urlquery/' + this.url), {
-        headers: { 'Content-Type': 'application/json' }
-        , observe: "body", responseType: "text" as "json"
-      }).subscribe(async data => {
 
-        this.data = new File([await data], 'Graph', { type: 'text', lastModified: Date.now() });
-
-        //data is taken as json and given to cytoscape's loadFile method as it is in navbar load
-        this._cyService.loadFile(this.data);
-      },
-        (err: HttpErrorResponse) => {
-          console.log(err.message);
-        }
-      );
-    }
-    else {
-      setTimeout(this.getData, 200);
-    }
+    this.http.get<any>(this.url, { responseType: 'blob' as 'json' }).subscribe(async data => {
+      this.data = new File([await data], 'Graph', { type: 'txt', lastModified: Date.now() });
+      //data is taken as json and given to cytoscape's loadFile method as it is in navbar load
+      this._cyService.loadFile(this.data);
+    },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
   }
-
 }
