ed9748115fe0f7a95639872e3aea0fd86eeabc12
canbax <yusufsaidcanbaz@gmail.com>
2020-09-18 19:05:29 +0300

    #278 custm query fixes

--- src/app/custom/queries/query1/query1.component.ts
+++ src/app/custom/queries/query1/query1.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Neo4jDb } from '../../../visuall/db-service/neo4j-db.service';
 import { CytoscapeService } from '../../../visuall/cytoscape.service';
 import { GlobalVariableService } from '../../../visuall/global-variable.service';
-import flatpickr from 'flatpickr';
 import { TableViewInput, TableDataType, TableFiltering, TableRowMeta } from '../../../shared/table-view/table-view-types';
 import { Subject } from 'rxjs';
 import { buildIdFilter, getOrderByExpression4Query, getQueryCondition4TxtFilter } from '../query-helper';
@@ -12,7 +11,7 @@ import { buildIdFilter, getOrderByExpression4Query, getQueryCondition4TxtFilter 
   templateUrl: './query1.component.html',
   styleUrls: ['./query1.component.css']
 })
-export class Query1Component implements OnInit, AfterViewInit {
+export class Query1Component implements OnInit {
 
   selectedGenre: string;
   movieGenres: string[];
@@ -21,8 +20,6 @@ export class Query1Component implements OnInit, AfterViewInit {
     resultCnt: 0, currPage: 1, pageSize: 0, isLoadGraph: true, isMergeGraph: true, isNodeData: true
   };
   tableFilled = new Subject<boolean>();
-  date1Id = 'query1-inp0';
-  date2Id = 'query1-inp1';
 
   constructor(private _dbService: Neo4jDb, private _cyService: CytoscapeService, private _g: GlobalVariableService) {
     this.movieGenres = [];
@@ -37,55 +34,39 @@ export class Query1Component implements OnInit, AfterViewInit {
     this._g.userPrefs.dataPageSize.subscribe(x => { this.tableInput.pageSize = x; });
   }
 
-  ngAfterViewInit(): void {
-    let opt = {
-      defaultDate: new Date(1960, 0, 1, 0, 0, 0), enableTime: true, enableSeconds: true, time_24hr: true,
-      minDate: this._g.userPrefs.dbQueryTimeRange.start.getValue(),
-      maxDate: this._g.userPrefs.dbQueryTimeRange.end.getValue(),
-    };
-    let opt2 = {
-      defaultDate: new Date(1969, 11, 31, 0, 0, 0), enableTime: true, enableSeconds: true, time_24hr: true,
-      minDate: this._g.userPrefs.dbQueryTimeRange.start.getValue(),
-      maxDate: this._g.userPrefs.dbQueryTimeRange.end.getValue(),
-    };
-
-    flatpickr('#' + this.date1Id, opt);
-    flatpickr('#' + this.date2Id, opt2);
-  }
-
   prepareQuery() {
-    const d1 = document.querySelector('#' + this.date1Id)['_flatpickr'].selectedDates[0].getFullYear();
-    const d2 = document.querySelector('#' + this.date2Id)['_flatpickr'].selectedDates[0].getFullYear();
     const skip = (this.tableInput.currPage - 1) * this.tableInput.pageSize;
-
-    this.getCountOfData(d1, d2);
-    this.loadTable(d1, d2, skip);
-    this.loadGraph(d1, d2, skip);
+    this.getCountOfData();
+    this.loadTable(skip);
+    this.loadGraph(skip);
   }
 
-  getCountOfData(d1: number, d2: number, filter?: TableFiltering) {
+  getCountOfData(filter?: TableFiltering) {
     const cb = (x) => { this.tableInput.resultCnt = x.data[0]; }
-    const txtCondition = getQueryCondition4TxtFilter(filter, ['m.primary_title']);
-    const cql = ` MATCH (m:Title)<-[:ACTOR|ACTRESS]-(:Person)
-    WHERE '${this.selectedGenre}' IN m.genres AND m.start_year> ${d1} AND m.start_year < ${d2} ${txtCondition} 
-    RETURN COUNT( DISTINCT m)`;
+    const txtCondition = getQueryCondition4TxtFilter(filter, ['n.primary_title']);
+    const dateFilter = this.getDateRangeCQL();
+
+    const cql = ` MATCH (n:Title)<-[:ACTOR|ACTRESS]-(:Person)
+    WHERE '${this.selectedGenre}' IN n.genres AND ${dateFilter} ${txtCondition} 
+    RETURN COUNT(DISTINCT n)`;
     this._dbService.runQuery(cql, cb, false);
   }
 
-  loadTable(d1: number, d2: number, skip: number, filter?: TableFiltering) {
+  loadTable(skip: number, filter?: TableFiltering) {
     const cb = (x) => this.fillTable(x);
-    const txtCondition = getQueryCondition4TxtFilter(filter, ['m.primary_title']);
-    const ui2Db = { 'Title': 'm.primary_title' };
-    const orderExpr = getOrderByExpression4Query(filter, 'm.primary_title', 'desc', ui2Db);
+    const txtCondition = getQueryCondition4TxtFilter(filter, ['n.primary_title']);
+    const ui2Db = { 'Title': 'n.primary_title' };
+    const orderExpr = getOrderByExpression4Query(filter, 'n.primary_title', 'desc', ui2Db);
+    const dateFilter = this.getDateRangeCQL();
 
-    const cql = ` MATCH (m:Title)<-[r:ACTOR|ACTRESS]-(:Person)
-    WHERE '${this.selectedGenre}' IN m.genres AND m.start_year > ${d1} AND m.start_year < ${d2} ${txtCondition} 
-    RETURN DISTINCT ID(m) as id, m.primary_title
+    const cql = ` MATCH (n:Title)<-[r:ACTOR|ACTRESS]-(:Person)
+    WHERE '${this.selectedGenre}' IN n.genres AND ${dateFilter} ${txtCondition} 
+    RETURN DISTINCT ID(n) as id, n.primary_title
     ORDER BY ${orderExpr} SKIP ${skip} LIMIT ${this.tableInput.pageSize}`;
     this._dbService.runQuery(cql, cb, false);
   }
 
-  loadGraph(d1: number, d2: number, skip: number, filter?: TableFiltering) {
+  loadGraph(skip: number, filter?: TableFiltering) {
     if (!this.tableInput.isLoadGraph) {
       return;
     }
@@ -93,9 +74,10 @@ export class Query1Component implements OnInit, AfterViewInit {
     const txtCondition = getQueryCondition4TxtFilter(filter, ['n.primary_title']);
     const ui2Db = { 'Title': 'n.primary_title' };
     const orderExpr = getOrderByExpression4Query(filter, 'n.primary_title', 'desc', ui2Db);
+    const dateFilter = this.getDateRangeCQL();
 
     const cql = `MATCH (n:Title)<-[r:ACTOR|ACTRESS]-(:Person)
-    WHERE '${this.selectedGenre}' IN n.genres AND n.start_year > ${d1} AND n.start_year < ${d2}  ${txtCondition}
+    WHERE '${this.selectedGenre}' IN n.genres AND ${dateFilter} ${txtCondition}
     WITH n, COLLECT(r) as edges
     RETURN  DISTINCT n, edges
     ORDER BY ${orderExpr} SKIP ${skip} LIMIT ${this.tableInput.pageSize}`;
@@ -119,8 +101,8 @@ export class Query1Component implements OnInit, AfterViewInit {
   }
 
   getDataForQueryResult(e: TableRowMeta) {
-    let d1 = document.querySelector('#' + this.date1Id)['_flatpickr'].selectedDates[0].getFullYear();
-    let d2 = document.querySelector('#' + this.date2Id)['_flatpickr'].selectedDates[0].getFullYear();
+    let d1 = this._g.userPrefs.dbQueryTimeRange.start.getValue();
+    let d2 = this._g.userPrefs.dbQueryTimeRange.end.getValue();
     const cb = (x) => this._cyService.loadElementsFromDatabase(x, this.tableInput.isMergeGraph)
 
     let idFilter = buildIdFilter(e.dbIds, true);
@@ -128,7 +110,7 @@ export class Query1Component implements OnInit, AfterViewInit {
     let orderExpr = getOrderByExpression4Query(null, 'n.primary_title', 'desc', ui2Db);
 
     let cql = `MATCH (n:Title)<-[r:ACTOR|ACTRESS]-(:Person)
-      WHERE '${this.selectedGenre}' IN n.genres AND ${idFilter}  n.start_year > ${d1} AND n.start_year < ${d2}
+      WHERE '${this.selectedGenre}' IN n.genres AND ${idFilter}  n.production_start_date > ${d1} AND n.production_end_date < ${d2}
       WITH n, COLLECT(r) as edges
       RETURN  DISTINCT n, edges
       ORDER BY ${orderExpr} SKIP 0 LIMIT ${this.tableInput.pageSize}`;
@@ -137,13 +119,21 @@ export class Query1Component implements OnInit, AfterViewInit {
 
   filterTable(filter: TableFiltering) {
     this.tableInput.currPage = 1;
-    let d1 = document.querySelector('#' + this.date1Id)['_flatpickr'].selectedDates[0].getFullYear();
-    let d2 = document.querySelector('#' + this.date2Id)['_flatpickr'].selectedDates[0].getFullYear();
-    this.getCountOfData(d1, d2, filter);
+    this.getCountOfData(filter);
     let skip = filter.skip ? filter.skip : 0;
-    this.loadTable(d1, d2, skip, filter);
+    this.loadTable(skip, filter);
     if (this.tableInput.isLoadGraph) {
-      this.loadGraph(d1, d2, skip, filter);
+      this.loadGraph(skip, filter);
     }
+  }
+
+  private getDateRangeCQL() {
+    const isLimit = this._g.userPrefs.isLimitDbQueries2range.getValue();
+    if (!isLimit) {
+      return 'TRUE';
+    }
+    const d1 = this._g.userPrefs.dbQueryTimeRange.start.getValue();
+    const d2 = this._g.userPrefs.dbQueryTimeRange.end.getValue();
+    return `n.production_start_date > ${d1} AND n.production_end_date < ${d2}`;
   }
 }
