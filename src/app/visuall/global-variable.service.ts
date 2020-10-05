294efc02f74fa7ae5b119445b7c7d06d2d8b1192
canbax <yusufsaidcanbaz@gmail.com>
2020-10-05 11:15:48 +0300

    #292 enable tiling in fcose

--- src/app/visuall/global-variable.service.ts
+++ src/app/visuall/global-variable.service.ts
@@ -264,7 +264,7 @@ export class GlobalVariableService {
       // Maximum number of iterations to perform
       numIter: 2500,
       // For enabling tiling
-      tile: false,
+      tile: true,
       // Represents the amount of the vertical space to put between the zero degree members during the tiling operation(can also be a function)
       tilingPaddingVertical: 10,
       // Represents the amount of the horizontal space to put between the zero degree members during the tiling operation(can also be a function)
