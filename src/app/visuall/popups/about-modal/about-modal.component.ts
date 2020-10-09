ec7ab62e5a1850f9e3a5f7cd083694bb300a65df
canbax <yusufsaidcanbaz@gmail.com>
2020-10-09 11:29:51 +0300

    #297 add timestamp after build

--- src/app/visuall/popups/about-modal/about-modal.component.ts
+++ src/app/visuall/popups/about-modal/about-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild, AfterViewChecked, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewChecked, ElementRef, OnDestroy } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 import { GlobalVariableService } from '../../global-variable.service';
 
 @Component({
@@ -7,29 +8,38 @@ import { GlobalVariableService } from '../../global-variable.service';
   templateUrl: './about-modal.component.html',
   styleUrls: ['./about-modal.component.css']
 })
-export class AboutModalComponent implements OnInit, AfterViewChecked {
+export class AboutModalComponent implements OnInit, AfterViewChecked, OnDestroy {
   @ViewChild('closeBtn', { static: false }) closeBtnRef: ElementRef;
   toolName: string;
   softwareVersion: string;
+  buildTime: string;
   toolLogo: string;
   companyName: string;
   companyContact: string;
+  subs: Subscription;
 
   constructor(public activeModal: NgbActiveModal, private _g: GlobalVariableService) { }
 
   ngOnInit() {
-    this._g.appDescription.subscribe(x => {
+    this.subs = this._g.appDescription.subscribe(x => {
       if (x) {
         this.toolName = x.appInfo.name;
         this.softwareVersion = x.appInfo.version;
         this.toolLogo = x.appInfo.icon;
         this.companyName = x.appInfo.company_name;
         this.companyContact = x.appInfo.company_contact;
+        this.buildTime = x.appInfo.build_time;
       }
     });
   }
 
   ngAfterViewChecked() {
     this.closeBtnRef.nativeElement.blur();
+  }
+
+  ngOnDestroy(): void {
+    if (this.subs) {
+      this.subs.unsubscribe();
+    }
   }
 }
---
+++ time-stamper.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+main();
+
+function main() {
+	let app_desc_file = 'dist/ng-visuall/app/custom/config/app_description.json';
+	if (process.argv.length === 3) {
+		app_desc_file = process.argv[2];
+	}
+
+	const app_desc = JSON.parse(readFile(app_desc_file));
+	app_desc.appInfo.build_time = new Date().toString();
+	writeFile(app_desc_file, JSON.stringify(app_desc));
+}
+
+function readFile(filename) {
+	if (typeof filename !== 'string') {
+		console.log('Invalid use of function!\nread_file(filename:string)');
+		return;
+	}
+
+	return fs.readFileSync(filename, 'utf8');
+}
+
+function writeFile(filename, content) {
+	if (typeof filename !== 'string' || typeof content !== 'string') {
+		console.log('Invalid use of function!\nwrite_file(filename:string, content:string)');
+		return;
+	}
+
+	fs.writeFile(filename, content, function (err) {
+		if (err) throw err;
+
+		console.log(filename + ' written successfully!');
+	});
+}
