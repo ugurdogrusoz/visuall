9117c882b3c2991353b8f8393b5c8e64a3996bb4
canbax <yusufsaidcanbaz@gmail.com>
2020-12-03 09:41:44 +0300

    format server.js, use compression

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const path = require('path');
 const https = require('https');
+const compression = require('compression');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text({ type: 'text/*' }))
-
+app.use(compression());
 
 const appPath = '/dist/ng-visuall';
 app.use(express.static(__dirname + appPath));
@@ -19,52 +20,48 @@ const port = process.env.PORT || 4200;
 app.listen(port);
 
 app.get('/urlquery/*', function (req, res) {
-	var reqURL = req.url.substr(10);
-	var data = [];
-	if (reqURL.substr(0, 5).toLowerCase() != "https" && reqURL.substr(0, 4).toLowerCase() == "http") {
-		var request = http.request(reqURL, function (response, body) {
-			response.on('data', function (chunk) {
-				data.push(chunk);
-			});
-			response.on('end', function () {
-				var result = JSON.parse(data.join(''))
-				res.send(result);
+  let reqURL = req.url.substr(10);
+  const data = [];
+  if (reqURL.substr(0, 5).toLowerCase() != "https" && reqURL.substr(0, 4).toLowerCase() == "http") {
+    let request = http.request(reqURL, function (response, body) {
+      response.on('data', function (chunk) {
+        data.push(chunk);
+      });
+      response.on('end', function () {
+        let result = JSON.parse(data.join(''))
+        res.send(result);
 
-			});
-		});
-		request.on('error', function (e) {
-			console.log(e.message);
-		});
-		request.end();
-	}
-	else {
-		if (reqURL.substr(0, 4).toLowerCase() != "http") {
-			reqURL = "https://" + reqURL;
-		}
-		var request = https.request(reqURL, function (response, body) {
-			response.on('data', function (chunk) {
-				data.push(chunk);
-			});
-			response.on('end', function () {
-				var result = JSON.parse(data.join(''))
-				res.send(result);
+      });
+    });
+    request.on('error', function (e) {
+      console.log(e.message);
+    });
+    request.end();
+  }
+  else {
+    if (reqURL.substr(0, 4).toLowerCase() != "http") {
+      reqURL = "https://" + reqURL;
+    }
+    let request = https.request(reqURL, function (response, body) {
+      response.on('data', function (chunk) {
+        data.push(chunk);
+      });
+      response.on('end', function () {
+        let result = JSON.parse(data.join(''))
+        res.send(result);
 
-			});
-		});
-		request.on('error', function (e) {
-			console.log(e.message);
-		});
-		request.end();
-	}
-
-
+      });
+    });
+    request.on('error', function (e) {
+      console.log(e.message);
+    });
+    request.end();
+  }
 });
 
 app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname + appPath + '/index.html'));
+  res.sendFile(path.join(__dirname + appPath + '/index.html'));
 });
 
-
-// Start the app by listening on the default Heroku port
-
+// Start the app by listening on the default port
 console.log('server listening port: ', port);
