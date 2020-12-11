055c593210cb17807ca58f152e1899ca41fb42dc
canbax <yusufsaidcanbaz@gmail.com>
2020-12-11 17:31:58 +0300

    bug fix for production build, remove deprecated extractCss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  dbConfig: {
+    url: 'http://localhost:7474/db/data/transaction/commit',
+    username: 'neo4j',
+    password: '123'
+  }
 };
