8361511f60a08c64d1a9f8d71abc4078f1c35662
canbax <yusufsaidcanbaz@gmail.com>
2021-01-26 21:59:52 +0300

    chage e2e baseUrl

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://visuall.herokuapp.com/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
