4cd33a15d3daf5035b5796b471dd93b1855f3f85
laramerdol <laramerdol@gmail.com>
2023-10-09 12:52:41 +0300

    Change the port of the visuall in server from 4200 (ocupied by hydra) to 4500 also fix database port

--- src/environments/environment.heroku.ts
+++ src/environments/environment.heroku.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   dbConfig: {
-    url: 'http://ivis.cs.bilkent.edu.tr:3001/db/data/transaction/commit',
+    url: 'http://ivis.cs.bilkent.edu.tr:3001/db/neo4j/tx/commit',
     username: 'neo4j',
     password: '123'
   }
