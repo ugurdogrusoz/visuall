797dca2ee7b32ef82cd6f174794de5aa32a68e72
laramerdol <laramerdol@gmail.com>
2023-10-25 15:41:30 +0300

    environment info fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   dbConfig: {
-    url: 'http://ivis.cs.bilkent.edu.tr:3001/db/neo4j/tx/commit',
+    url: 'http://ivis.cs.bilkent.edu.tr:3004/db/neo4j/tx/commit',
     username: 'neo4j',
     password: '12345678'
   }
